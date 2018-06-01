8cdfeba9a8f1d4f5aa8ac0befa56669ee141f898
adityab7 <adityab7@educbe.ca>
2018-05-31 20:13:11 -0700

    ..
    ..

--- js/modal.js
+++
@@ -1,8 +0,0 @@
-function clickedModal(modalName) {
-    var modal = document.getElementById(modalName);
-    if (modal.style.display === "block") {
-        modal.style.display = "none";
-    } else {
-        modal.style.display = "block";
-    }
-}
